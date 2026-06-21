083407dc108bfbae88e147cc95e3c720623f0e16
Mann-Wahrenberg, Alexander (RDS-RM) <Alexander.Mann-Wahrenberg@marquardt.com>
2026-06-21 09:12:40 +0200

    Add line follower source code

---
+++ docs/03-construction-phase/_listings/line-follower.cpp
@@ -0,0 +1,83 @@
+const int PIN_SENSOR_LINKS  = 2;
+const int PIN_SENSOR_RECHTS = 3;
+
+const int PIN_MOTOR_LINKS = 45;
+const int PIN_MOTOR_RECHTS = 42;
+
+int helligkeit_links;
+int helligkeit_rechts;
+
+String untergrund_links = "Unbekannt";
+String untergrund_rechts = "Unbekannt";
+
+String untergrund_neu(String untergrund_alt, int helligkeit) {
+  if(helligkeit > 4000)
+    return "SCHWARZ";
+  else if (helligkeit < 3900)
+    return "WEISS";
+  else
+    return untergrund_alt;
+}
+
+String ort = "ende";
+
+String ort_neu(String ort_alt, String untergrund_links, String untergrund_rechts){
+  if (untergrund_rechts == "SCHWARZ" && untergrund_links == "SCHWARZ")
+    return "ende";
+  else if (untergrund_rechts == "SCHWARZ")
+      return "links";
+  else if (untergrund_links == "SCHWARZ")
+      return "rechts";
+  else
+      return ort_alt;
+}
+
+const int GESCHWINDIGKEIT_NORMAL = 150;
+const int GESCHWINDIGKEIT_UNTERSCHIED = 30;
+
+void fahre(String ort){
+  if (ort == "ende") {
+    analogWrite(PIN_MOTOR_LINKS, 0);
+    analogWrite(PIN_MOTOR_RECHTS, 0);
+  } else if (ort == "links"){
+    analogWrite(PIN_MOTOR_LINKS, GESCHWINDIGKEIT_NORMAL + GESCHWINDIGKEIT_UNTERSCHIED);
+    analogWrite(PIN_MOTOR_RECHTS, GESCHWINDIGKEIT_NORMAL - GESCHWINDIGKEIT_UNTERSCHIED);
+  } else if (ort == "rechts") {
+    analogWrite(PIN_MOTOR_LINKS, GESCHWINDIGKEIT_NORMAL - GESCHWINDIGKEIT_UNTERSCHIED);
+    analogWrite(PIN_MOTOR_RECHTS, GESCHWINDIGKEIT_NORMAL + GESCHWINDIGKEIT_UNTERSCHIED);
+  } else {
+    analogWrite(PIN_MOTOR_LINKS, GESCHWINDIGKEIT_NORMAL);
+    analogWrite(PIN_MOTOR_RECHTS, GESCHWINDIGKEIT_NORMAL);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  helligkeit_links = analogRead(PIN_SENSOR_LINKS);
+  helligkeit_rechts = analogRead(PIN_SENSOR_RECHTS);
+
+  Serial.print(helligkeit_links);
+  Serial.print(" ");
+  Serial.println(helligkeit_rechts);
+
+  untergrund_links = untergrund_neu(untergrund_links, helligkeit_links);
+  untergrund_rechts = untergrund_neu(untergrund_rechts, helligkeit_rechts);
+
+  Serial.println(untergrund_links);
+  Serial.println(untergrund_rechts);
+
+  ort = ort_neu(ort, untergrund_links, untergrund_rechts);
+
+  Serial.println(ort);
+  Serial.println();
+
+  fahre(ort);
+
+  delay(100);
+
+
+}
+
